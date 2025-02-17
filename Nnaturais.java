39c3ded93afee51e177fbf8445ee77d55f394b82
Cauã Henrique Ramos Rezende <140010145+Cauahenrique08@users.noreply.github.com>
2025-02-16 21:50:03 -0500

    Update Nnaturais.java

--- Nnaturais.java
+++ Nnaturais.java
@@ -9,11 +9,11 @@ public class Nnaturais
   
   static public int somatoria (int N) 
   { 
-	 //condição de parada
+	 //condição de parada caso o número seja 0 não adicionará nenhum conteudo a somatória, com isso retornando o valor
 	  if (N == 0)
 	  {return 0;}
 	  else 
-	  {  return N = N + somatoria(N-1);
+	  {  return N = N + somatoria(N-1);  // retornando todos os valores da recursivdade
 		  
 	  }
 	      
